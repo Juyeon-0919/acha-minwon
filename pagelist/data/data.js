0ff819acb7990eece18329802c85fa6671fb5199
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-03-29 12:54:20 +0900

    아차 - 민원센터 퍼블리싱 완료

--- pagelist/data/data.js
+++ pagelist/data/data.js
@@ -1,12 +1,103 @@
 const data = {}
 data.common = {
-  title: '메인',
+  title: '공통',
   list: [
     {
       stateClass: 'complete',
-      url: 'error',
-      category: '메인',
-      page: '에러 페이지'
+      url: 'layout',
+      category: '',
+      page: '레이아웃'
+    },{
+      stateClass: 'complete',
+      url: 'component',
+      category: '',
+      page: '컴포넌트'
+    }
+  ]
+}
+data.minwon = {
+  title: '민원센터',
+  list: [
+    {
+      stateClass: 'complete',
+      url: '1. 민원센터 메인',
+      category: '',
+      page: '1. 민원센터 메인'
+    },{
+      stateClass: 'complete',
+      url: '2. FAQ',
+      category: '',
+      page: '2. FAQ'
+    },{
+      stateClass: 'complete',
+      url: '3. 개인정보 수집동의',
+      category: '',
+      page: '3. 개인정보 수집동의'
+    },{
+      stateClass: 'complete',
+      url: '4. 민원 신청서 작성',
+      category: '',
+      page: '4. 민원 신청서 작성'
+    },{
+      stateClass: 'complete',
+      url: '5. 민원 신청서 완료',
+      category: '',
+      page: '5. 민원 신청서 완료'
+    },{
+      stateClass: 'complete',
+      url: '6. 나의민원 진입',
+      category: '',
+      page: '6. 나의민원 진입'
+    },{
+      stateClass: 'complete',
+      url: '7. 신청한 민원 진행 현황 조회',
+      category: '',
+      page: '7. 신청한 민원 진행 현황 조회'
+    },{
+      stateClass: 'complete',
+      url: '8-1. 민원인 추가 보완요청',
+      category: '',
+      page: '8-1. 민원인 추가 보완요청'
+    },{
+      stateClass: 'complete',
+      url: '8-2. 민원인 추가 보완요청 (제출완료)',
+      category: '',
+      page: '8-2. 민원인 추가 보완요청 (제출완료)'
+    },{
+      stateClass: 'complete',
+      url: '9. 이의 신청한 민원 진행 현황 조회',
+      category: '',
+      page: '9. 이의 신청한 민원 진행 현황 조회'
+    },{
+      stateClass: 'complete',
+      url: '10. 민원신청, 민원접수 (취하팝업)',
+      category: '',
+      page: '10. 민원신청, 민원접수 (취하팝업)'
+    },{
+      stateClass: 'complete',
+      url: '10. 민원신청 상태인 경우 상세 화면',
+      category: '',
+      page: '10. 민원신청 상태인 경우 상세 화면'
+    },{
+      stateClass: 'complete',
+      url: '10. 취하완료',
+      category: '',
+      page: '10. 취하완료'
+    },{
+      stateClass: 'complete',
+      url: '11. 민원 신청서 작성(팝업)',
+      category: '',
+      page: '11. 민원 신청서 작성(팝업)'
+    },{
+      stateClass: 'complete',
+      url: '11. 민원 신청서 작성',
+      category: '',
+      page: '11. 민원 신청서 작성'
+    },{
+      stateClass: 'complete',
+      url: '12. 민원 신청서 작성',
+      category: '',
+      page: '12. 민원 신청서 작성'
     }
   ]
 }
